1ea2f15333c4961d560c5f1c150a2d65e2dffaea
Issei Nagase <e235732@ie.u-ryukyu.ac.jp>
2023-11-14 00:47:36 +0900

    Enemy.javaとHero.javaにJavaDocコメントを追加し、HTMLドキュメントを生成

--- app/src/main/java/jp/ac/uryukyu/ie/e235732/Hero.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e235732/Hero.java
@@ -2,19 +2,27 @@ package jp.ac.uryukyu.ie.e235732;
 
 /**
  * ヒーロークラス。
- *  String name; //敵の名前
- *  int hitPoint; //敵のHP
- *  int attack; //敵の攻撃力
- *  boolean dead; //敵の生死状態。true=死亡。
- * Created by tnal on 2016/11/13.
+ * ゲームの中で勇者を表すクラスです。
+ *
+ * <p>ヒーローは名前、HP、攻撃力、生死状態の情報を持ちます。</p>
+ * <p>攻撃メソッドやダメージ処理メソッドを提供しています。</p>
+ *
+ * @author tnal
+ * @version 1.0
  */
-
 public class Hero {
     private String name;
     private int hitPoint;
     private int attack;
     private boolean dead;
 
+    /**
+     * ヒーロークラスのコンストラクタ。
+     *
+     * @param name       ヒーローの名前
+     * @param maximumHP  ヒーローの最大HP
+     * @param attack     ヒーローの攻撃力
+     */
     public Hero(String name, int maximumHP, int attack) {
         this.name = name;
         hitPoint = maximumHP;
@@ -23,28 +31,58 @@ public class Hero {
         System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, maximumHP, attack);
     }
 
+    /**
+     * ヒーローの名前を取得するメソッド。
+     *
+     * @return ヒーローの名前
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * ヒーローの現在のHPを取得するメソッド。
+     *
+     * @return ヒーローの現在のHP
+     */
     public int getHitPoint() {
         return hitPoint;
     }
 
+    /**
+     * ヒーローの攻撃力を取得するメソッド。
+     *
+     * @return ヒーローの攻撃力
+     */
     public int getAttack() {
         return attack;
     }
 
+    /**
+     * ヒーローが生存しているかどうかを取得するメソッド。
+     *
+     * @return ヒーローが死亡していれば true、生存していれば false
+     */
     public boolean isDead() {
         return dead;
     }
 
+    /**
+     * 敵に対して攻撃を行うメソッド。
+     *
+     * @param enemy 攻撃対象の敵
+     */
     public void attack(Enemy enemy) {
         int damage = (int) (Math.random() * attack);
         System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, enemy.getName(), damage);
         enemy.wounded(damage);
     }
 
+    /**
+     * ダメージを受けたときの処理を行うメソッド。
+     *
+     * @param damage 受けたダメージ
+     */
     public void wounded(int damage) {
         hitPoint -= damage;
         if (hitPoint < 0) {
